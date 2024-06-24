52dca290cc4affb6d910f37125ae1593fce4ec16
sranucci <sranucci@itba.edu.ar>
2024-06-24 20:22:20 -0300

    Final commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyB84lGRGJZgQ-9m_0f3Kn6E2YWskqIsQ3Y",
+    authDomain: "fir-auth-graphql-2d4eb.firebaseapp.com",
+    projectId: "fir-auth-graphql-2d4eb",
+    storageBucket: "fir-auth-graphql-2d4eb.appspot.com",
+    messagingSenderId: "623336785373",
+    appId: "1:623336785373:web:a72b6b38ac1579c19ca234"
+  };
